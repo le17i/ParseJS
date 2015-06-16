35d8496e407f3bff0c12f1b81bfa2735301f98a0
Leandro Dias <le2.17@hotmail.com>
2015-06-16 16:12:39 -0300

    Helpers namespace

--- parse.js
+++ parse.js
@@ -42,15 +42,15 @@
 
    parse.fn.toNumber = function(precision) {
 
-      var value = helpers.precisionNumber(this.value, precision);
+      var value = helpers.format.precision(this.value, precision);
 
-      return helpers.thousandsNumberSeparator(value);
+      return helpers.format.thousand(value);
    };
 
    parse.fn.toCurrency = function() {
 
-      var value = helpers.precisionNumber(this.value, 2);
-      value = helpers.thousandsNumberSeparator(value);
+      var value = helpers.format.precision(this.value, 2);
+      value = helpers.format.thousand(value);
 
       return [parse.config.currencySymbol, value].join("");
    };
@@ -64,53 +64,55 @@
       thousandsSeparator: /(\d)(?=(\d{3})+(?!\d))/g
    }
 
-   helpers.precisionNumber = function(value, precision) {
-      if(!value || !precision) return "Invalid value";
+   helpers.format = {
+      precision: function(value, precision) {
+         if(!value || !precision) return "Invalid value";
 
-      var v = value.toString();
+         var v = value.toString();
 
-      if(v.indexOf(parse.config.decimalSeparator) > -1) {
-         var length = -((v.length - v.indexOf(parse.config.decimalSeparator)) - 1) + precision;
+         if(v.indexOf(parse.config.decimalSeparator) > -1) {
+            var length = -((v.length - v.indexOf(parse.config.decimalSeparator)) - 1) + precision;
 
-         if(length > 0) {
-            v = v.substring(0, (v.indexOf(parse.config.decimalSeparator) + 1) + precision);
+            if(length > 0) {
+               v = v.substring(0, (v.indexOf(parse.config.decimalSeparator) + 1) + precision);
+            }
+            else {
+               var a = 0;
+               for(a; a < length; a++) {
+                  v = [v, "0"].join("");
+               }
+            }
          }
          else {
+            v = [v, parse.config.decimalSeparator].join("");
+
             var a = 0;
-            for(a; a < length; a++) {
+            for(a; a < precision; a++) {
                v = [v, "0"].join("");
             }
          }
-      }
-      else {
-         v = [v, parse.config.decimalSeparator].join("");
 
-         var a = 0;
-         for(a; a < precision; a++) {
-            v = [v, "0"].join("");
+         return v;
+      },
+
+      thousand: function(value) {
+         if(!value) return "Invalid value";
+
+         var v = value.toString();
+         var replace = ["$1", parse.config.thousandSeparator].join("");
+
+         if(v.indexOf(parse.config.decimalSeparator) > -1) {
+            var vInt = v.substring(0, v.indexOf(parse.config.decimalSeparator));
+
+            vInt = vInt.replace(helpers.regex.thousandsSeparator, replace);
+            v = [vInt, v.substring(v.indexOf(parse.config.decimalSeparator) + 1, v.length)].join(parse.config.decimalSeparator);
          }
+         else {
+            v = v.replace(helpers.regex.thousandsSeparator, replace);
+         }
+
+         return v;
       }
-
-      return v;
-   };
-
-   helpers.thousandsNumberSeparator = function(value) {
-      if(!value) return "Invalid value";
-
-      var v = value.toString();
-      var replace = ["$1", parse.config.thousandSeparator].join("");
-
-      if(v.indexOf(parse.config.decimalSeparator) > -1) {
-         var vInt = v.substring(0, v.indexOf(parse.config.decimalSeparator));
-
-         vInt = vInt.replace(helpers.regex.thousandsSeparator, replace);
-         v = [vInt, v.substring(v.indexOf(parse.config.decimalSeparator) + 1, v.length)].join(parse.config.decimalSeparator);
-      }
-      else {
-         v = v.replace(helpers.regex.thousandsSeparator, replace);
-      }
-
-      return v;
    };
 
    window.parse = parse;
