94a1be3595fb24d4f636b7e716ffc032bc16c468
Leandro Dias <le2.17@hotmail.com>
2015-06-16 16:08:17 -0300

    Add parseJS file'

---
+++ parse.js
@@ -0,0 +1,118 @@
+;(function(window, document, undefined) {
+
+   "use strict";
+
+   var parse = function(value) {
+      return new parse.fn.init(value);
+   };
+
+   parse.config = {
+      currencySymbol: "R$",
+      decimalSeparator: ",",
+      thousandSeparator: ".",
+   };
+
+   parse.set = function(key, value) {
+      if(!key || !value || !parse.config.hasOwnPropery(key)) return;
+
+      parse.config[key] = value;
+   };
+
+   parse.fn = parse.prototype = {
+      init: function(value) {
+         this.value = value;
+         return this;
+      },
+      value: null
+   };
+
+   parse.fn.init.prototype = parse.prototype;
+
+   //parse.fn.toDate = function(format) {
+   //   return format;
+   //};
+
+   parse.fn.toPercent = function(precision) {
+
+      var value = helpers.precisionNumber(this.value, precision);
+      value = helpers.thousandsNumberSeparator(value);
+
+      return [value, "%"].join("");
+   };
+
+   parse.fn.toNumber = function(precision) {
+
+      var value = helpers.precisionNumber(this.value, precision);
+
+      return helpers.thousandsNumberSeparator(value);
+   };
+
+   parse.fn.toCurrency = function() {
+
+      var value = helpers.precisionNumber(this.value, 2);
+      value = helpers.thousandsNumberSeparator(value);
+
+      return [parse.config.currencySymbol, value].join("");
+   };
+
+   /**
+   *  Helpers Functions
+   */
+   var helpers = {};
+
+   helpers.regex = {
+      thousandsSeparator: /(\d)(?=(\d{3})+(?!\d))/g
+   }
+
+   helpers.precisionNumber = function(value, precision) {
+      if(!value || !precision) return "Invalid value";
+
+      var v = value.toString();
+
+      if(v.indexOf(parse.config.decimalSeparator) > -1) {
+         var length = -((v.length - v.indexOf(parse.config.decimalSeparator)) - 1) + precision;
+
+         if(length > 0) {
+            v = v.substring(0, (v.indexOf(parse.config.decimalSeparator) + 1) + precision);
+         }
+         else {
+            var a = 0;
+            for(a; a < length; a++) {
+               v = [v, "0"].join("");
+            }
+         }
+      }
+      else {
+         v = [v, parse.config.decimalSeparator].join("");
+
+         var a = 0;
+         for(a; a < precision; a++) {
+            v = [v, "0"].join("");
+         }
+      }
+
+      return v;
+   };
+
+   helpers.thousandsNumberSeparator = function(value) {
+      if(!value) return "Invalid value";
+
+      var v = value.toString();
+      var replace = ["$1", parse.config.thousandSeparator].join("");
+
+      if(v.indexOf(parse.config.decimalSeparator) > -1) {
+         var vInt = v.substring(0, v.indexOf(parse.config.decimalSeparator));
+
+         vInt = vInt.replace(helpers.regex.thousandsSeparator, replace);
+         v = [vInt, v.substring(v.indexOf(parse.config.decimalSeparator) + 1, v.length)].join(parse.config.decimalSeparator);
+      }
+      else {
+         v = v.replace(helpers.regex.thousandsSeparator, replace);
+      }
+
+      return v;
+   };
+
+   window.parse = parse;
+
+})(window, document);
